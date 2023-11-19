b2104c317b6b4c6f519d77c6113a7f7b631946d0
Bharath Lakshman Kumar <108531789+BharathxD@users.noreply.github.com>
2023-11-19 21:15:13 +0530

    chore: improve variable naming for better readability

--- struct-impl/src/main.rs
+++ struct-impl/src/main.rs
@@ -11,13 +11,13 @@ impl Rectangle {
 }
 
 fn main() {
-    let rect1 = Rectangle {
+    let rectangle = Rectangle {
         width: 30,
         height: 50,
     };
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        rect1.area()
+        rectangle.area()
     );
 }
