a34d1b87a1bb08295facd3825392e5622d40b86d
Bharath Lakshman Kumar <108531789+BharathxD@users.noreply.github.com>
2023-11-27 23:22:26 +0530

    chore: properly type `array_example`

--- types/src/main.rs
+++ types/src/main.rs
@@ -34,7 +34,7 @@ fn main() {
     // Unlike arrays in other languages, in rust the arrays will have fixed length and contains only one type
     let _array = [1, 2, 3, 4, 5];
     // You can use `vector`, which is allowed to grow and shrink in size, but array can be very useful for constants like names of the months
-    let _months = ["January", "February", "March", "April", "May", "June", "July",
+    let _months: [&str; 12]  = ["January", "February", "March", "April", "May", "June", "July",
               "August", "September", "October", "November", "December"];
     // Other useful type specifications
     let array_example: [i32; 5] = [1, 2, 3, 4, 5];
