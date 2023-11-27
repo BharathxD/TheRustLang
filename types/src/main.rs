e4b26e87fa9ec652af139ac5beb535825a63590d
Bharath Lakshman Kumar <108531789+BharathxD@users.noreply.github.com>
2023-11-27 23:25:09 +0530

    chore: properly type `tuples`

--- types/src/main.rs
+++ types/src/main.rs
@@ -25,7 +25,7 @@ fn main() {
     // Tuples have fixed length and can have multiple types
     let tup: (i32, f64, u8) = (500, 6.4, 1);
     // Tuple descructuring
-    let (_x, _y, _z) = tup;
+    let (_x, _y, _z): (i32, f64, u8) = tup;
     let _five_hundred = tup.0;
     let _six_point_four = tup.1;
     let _one = tup.2;
