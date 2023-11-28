dede3265ff2ec70dcb5f19daa8fbbb621ce422fb
Bharath Lakshman Kumar <108531789+BharathxD@users.noreply.github.com>
2023-11-28 22:59:27 +0530

    fea: use `match` for better readability

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,39 +1,63 @@
-fn main() {
-    fibonacci();
-}
+use std::io;
 
+/// Calculates and prints the Fibonacci sequence up to the specified term.
+///
+/// # Examples
+///
+/// ```
+/// fibonacci();
+/// ```
 fn fibonacci() {
-    let mut n: String = String::new();
-    std::io::stdin().read_line(&mut n).expect("Error reading the input");
-    let n = n.trim().parse().expect("Invalid type given as the input in cli");
+    // Read input from the user
+    let mut n = String::new();
+    io::stdin().read_line(&mut n).expect("Error reading the input");
+
+    // Parse input to an integer
+    let n: i32 = n.trim().parse().expect("Invalid type given as the input in CLI");
+
+    // Initialize Fibonacci sequence variables
     let mut t1 = 0i32;
     let mut t2 = 1i32;
     let mut next_val = 0;
     let mut i = 1i32;
+
+    // Calculate and print the Fibonacci sequence
     while i <= n {
-        if i == 1 {
-            println!("0");
-            i += 1;
-            continue;
+        match i {
+            1 => println!("0"),
+            2 => println!("1"),
+            _ => {
+                next_val = t1 + t2;
+                t1 = t2;
+                t2 = next_val;
+                println!("{}", next_val);
+            }
         }
-        if i == 2 {
-            println!("1");
-            i += 1;
-            continue;
-        }
-        next_val = t1 + t2;
-        t1 = t2;
-        t2 = next_val;
-        println!("{}", next_val);
         i += 1;
     }
 }
 
+/// Performs a countdown from the user-specified number to zero.
+///
+/// # Examples
+///
+/// ```
+/// _countdown();
+/// ```
 fn _countdown() {
+    // Read input from the user
     let mut countdown_from = String::new();
-    std::io::stdin().read_line(&mut countdown_from).expect("Error reading the input");
+    io::stdin().read_line(&mut countdown_from).expect("Error reading the input");
+
+    // Parse input to an integer
     let countdown_from: i32 = countdown_from.trim().parse().expect("Invalid type");
-    for count in (0..(countdown_from + 1)).rev() {
+
+    // Perform countdown and print each count
+    for count in (0..=countdown_from).rev() {
         println!("Counting: {}", count);
     }
+}
+
+fn main() {
+    fibonacci();
 }
