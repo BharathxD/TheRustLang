087f4cd1e97a5c0d401a1165462bc72e32b661e7
Bharath Lakshman Kumar <108531789+BharathxD@users.noreply.github.com>
2023-11-30 23:13:14 +0530

    refactor: add comments for improved code documentation

--- ownership-lifetime/src/main.rs
+++ ownership-lifetime/src/main.rs
@@ -1,19 +1,31 @@
 #[derive(Debug)]
 struct Counter {
-    count: i32
+    count: i32,
 }
 
+// Function to print the counter using the Debug trait.
 fn print_counter(counter: &Counter) {
     println!("{:?}", counter);
 }
 
+// Function to find and return the reference to the Counter with the highest count.
 fn highest_counter<'a>(x: &'a Counter, y: &'a Counter) -> &'a Counter {
-    if x.count > y.count { x } else { y }
+    // Returns the reference to the Counter with the highest count.
+    if x.count > y.count {
+        x
+    } else {
+        y
+    }
 }
 
 fn main() {
+    // Creating instances of the Counter struct.
     let a: Counter = Counter { count: 1 };
     let b: Counter = Counter { count: 2 };
+
+    // Finding the reference to the Counter with the highest count.
     let highest: &Counter = highest_counter(&a, &b);
+
+    // Printing the counter with the highest count.
     print_counter(&highest);
 }
