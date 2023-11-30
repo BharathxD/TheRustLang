5ce597e846804f67852334df7e80843c48b15598
Bharath Lakshman Kumar <108531789+BharathxD@users.noreply.github.com>
2023-11-30 23:14:41 +0530

    feat: improve age categorization in main function

--- match-example/src/main.rs
+++ match-example/src/main.rs
@@ -1,9 +1,12 @@
 fn main() {
+    // Age variable representing an individual's age
     let age: i32 = 8;
+
+    // Match statement to categorize age ranges
     match age {
         1..=18 => println!("Less than or equal to 18"),
         40 | 50 => println!("The age is 40 (or) 50"),
         65..=i32::MAX => println!("The age is above 65"),
-        _ => println!("Not sure")
+        _ => println!("Not sure"),
     }
 }
