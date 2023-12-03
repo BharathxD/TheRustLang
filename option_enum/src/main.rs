f9196ac0cd5072fba8646c2c55c16c7f7ac5f7c5
Bharath Lakshman Kumar <108531789+BharathxD@users.noreply.github.com>
2023-12-03 18:43:55 +0530

    feat: example syntac of `option_enum`

--- option_enum/src/main.rs
+++ option_enum/src/main.rs
@@ -1,3 +1,9 @@
+// Example Syntax
+// enum Option<T> {
+//     Some(T),
+//     None
+// }
+
 fn main() {
     let some_number: Option<i32> = Some(1);
     let some_string: Option<&str> = Some("Hello, world!");
