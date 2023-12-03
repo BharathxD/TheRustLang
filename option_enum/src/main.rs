029d8727519e09ef4e767f82552d0f1e86cc3e59
Bharath Lakshman Kumar <108531789+BharathxD@users.noreply.github.com>
2023-12-03 19:08:39 +0530

    feat: use the match expression

--- option_enum/src/main.rs
+++ option_enum/src/main.rs
@@ -48,3 +48,10 @@ fn value_in_cents(coin: Coin) -> u8 {
         Coin::Quarter => 25,
     };
 }
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
