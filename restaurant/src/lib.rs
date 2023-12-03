3d9a4b0331a2d00bf918f61a8ab3c7f0d692f4f4
Bharath Lakshman Kumar <108531789+BharathxD@users.noreply.github.com>
2023-12-03 19:27:58 +0530

    feat: create a crate/lib named `restaurant`

---
+++ restaurant/src/lib.rs
@@ -0,0 +1,20 @@
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
+
+mod front_of_house {
+    mod hosting {
+        fn add_to_waitlist() {}
+    }
+}
