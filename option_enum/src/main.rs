c0605371221fabf86b842ab4038fe02635b1d641
Bharath Lakshman Kumar <108531789+BharathxD@users.noreply.github.com>
2023-12-03 19:00:09 +0530

    feat: add pattern to bind values

--- option_enum/src/main.rs
+++ option_enum/src/main.rs
@@ -4,6 +4,13 @@
 //     None
 // }
 
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter
+}
+
 fn main() {
     let x: i8 = 1;
     let y: Option<i8> = Some(1);
@@ -18,4 +25,13 @@ fn _syntax() {
     let _some_string: Option<&str> = Some("Hello, world!");
 
     let _none: Option<i32> = None;
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    return match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    };
 }
