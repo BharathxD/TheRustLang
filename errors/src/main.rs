13f8861edac9ccfbf69b76d16927ba7cb2eb961b
Bharath Lakshman Kumar <108531789+BharathxD@users.noreply.github.com>
2023-12-10 13:13:21 +0530

    feat: handle errors gracefully using `Result` enum

--- errors/src/main.rs
+++ errors/src/main.rs
@@ -1,7 +1,35 @@
+use std::fs::File;
+use std::io::Error;
+use std::io::ErrorKind;
+
 fn main() {
-    panic_macro();
+    handle_grace();
 }
 
 fn panic_macro() {
+    // If you want make the program exit, in the scenarios where the errors cannot be handled gracefully
     panic!("Crash and burn");
+}
+
+fn handle_grace() {
+    // It's just the option enum
+    // enum Result<T, E> {
+    //     Ok(T),
+    //     Err(E),
+    // }
+
+    let f: Result<File, Error> = File::open("hello.txt");
+
+    let f: File = match f {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the File: {:?}", e),
+            },
+            other_error => {
+                panic!("Problem creating the File: {:?}", other_error)
+            }
+        },
+    };
 }
