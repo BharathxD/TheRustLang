14e4312238fd9c7f206ff8599cd22f4fd8a2a4fb
Bharath Lakshman Kumar <108531789+BharathxD@users.noreply.github.com>
2023-12-15 17:17:23 +0530

    feat: add generics for enums

--- rust-generics/src/main.rs
+++ rust-generics/src/main.rs
@@ -1,4 +1,33 @@
 fn main() {
+    struct_generic_example();
+    vec_generic_example();
+    enum_example();
+}
+
+fn enum_example() {
+    enum Options<T> {
+        Some(T),
+        None,
+    }
+    enum Result<T, E> {
+        Ok(T),
+        Err(E),
+    }
+}
+
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+fn struct_generic_example() {
+    let integer = Point { x: 5, y: 10 };
+    let float = Point { x: 1.0, y: 4.0 };
+    println!("integer.x = {}", integer.x);
+    println!("float.x = {}", float.x);
+}
+
+fn vec_generic_example() {
     let number_list = vec![34, 50, 25, 100, 65];
     let chat_list = vec!['y', 'm', 'a', 'q'];
     println!("The largest number is {}", find_largest(number_list));
