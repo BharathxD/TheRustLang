6b580784cf8df5e0f52eded0009a558d2c80c16a
Bharath Lakshman Kumar <108531789+BharathxD@users.noreply.github.com>
2023-12-15 18:04:07 +0530

    feat: add `user`, `trait bounds`

--- traits/src/main.rs
+++ traits/src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt::Debug;
+
 pub struct NewsArticle {
     pub author: String,
     pub headline: String,
@@ -29,6 +31,34 @@ impl Summary for Tweet {
     }
 }
 
+// The notify function takes in an argument/paramenter named item, it accepts anything that implements the Summary trait.
+// It is actually a syntact suger for trait bound syntax
+// pub fn notify(item: &impl Summary) {
+//     println!("Breaking news! {}", item.summarize())
+// }
+
+// Trait bound syntax
+// pub fn notify<T: Summary>(item: &T) {
+//     println!("Breaking news! {}", item.summarize())
+// }
+
+// More complicated syntax where multiple trait bounds are required
+// pub fn notify(item1: &impl Summary, item2: &impl Summary) {}
+pub fn notify<T: Summary>(item1: &T, item2: &T) {
+    for item in vec![item1, item2] {
+        println!("Breaking news! {}", item.summarize())
+    }
+}
+
+// fn some_function<T: Summary + Clone, U: Clone + Debug>(t: &T, u: &U) -> i32 {}
+fn some_function<T, U>(t: &T, u: &U) -> i32
+where
+    T: Summary + Clone,
+    U: Clone + Debug,
+{
+    0
+}
+
 fn main() {
     let tweet = Tweet {
         username: String::from("horse_ebooks"),
@@ -45,4 +75,6 @@ fn main() {
 
     println!("1 new tweet from {}", tweet.summarize());
     println!("New article available! {}", article.summarize());
+
+    // notify(&tweet);
 }
