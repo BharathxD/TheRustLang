c75b6472d811cfd3907f34940197ec0a3f84e68a
Bharath Lakshman Kumar <108531789+BharathxD@users.noreply.github.com>
2023-12-23 22:57:42 +0530

    chore(refactor): Improve variable names and comments

--- common_collection/src/main.rs
+++ common_collection/src/main.rs
@@ -1,27 +1,23 @@
 use std::collections::HashMap;
 
 fn main() {
-    // Normal array data type
+    // Declare a normal array of integers
     let _a: [i32; 4] = [1, 2, 3, 4];
 
+    // Declare a mutable vector and assign values to it
     let mut v: Vec<i32> = Vec::new();
-    // One way to assign values to a vector
     v.push(1);
     v.push(2);
     v.push(3);
     v.push(4);
 
-    // Another way to assign value to a vector, similar to array (with vec! macro)
-    //? The vector is stored in a `heap` and is scoped
+    // Another way to assign values to a vector using the vec! macro
     let mut v2: Vec<i32> = vec![1, 2, 3, 4];
 
-    // Access elements inside of a vector
-    // Use index to access the value in that index
+    // Access elements inside of a vector using index
     let _fourth = &v2[3];
 
-    // Invalid index
-    // let fourth = &v2[20];
-    // How to fix this index out of range error? Look at the following pattern
+    // Use match to handle potential index out of range error
     match v2.get(3) {
         Some(fourth) => println!("The fourth element is: {}", fourth),
         None => println!("There is no fourth element!"),
@@ -38,93 +34,96 @@ fn main() {
     }
 }
 
-// Store different types of data in a vector
+// Function to demonstrate storing different types of data in a vector
 fn vec() {
+    // Define an enum to represent different types of spreadsheet cells
     enum SpreadSheetCell {
         Int(i32),
         Float(f64),
         Text(String),
     }
 
+    // Create a vector of different types of spreadsheet cells
     let row = vec![
         SpreadSheetCell::Int(20),
         SpreadSheetCell::Float(19.8),
         SpreadSheetCell::Text(String::from("Hello, world!")),
     ];
 
+    // Use match to handle different types of spreadsheet cells
     match &row[1] {
         SpreadSheetCell::Int(i) => println!("{} is an Integer", i),
         _ => println!("Not an Integer"),
     }
 }
 
+// Function to demonstrate string operations
 fn strings() {
-    // Strings are stored as a collection of UTF-8 encode bytes
+    // Declare different types of strings
     let string_1: String = String::new();
     let string_2: &str = "Initial Contents";
     let string_3: String = string_2.to_string();
     let string_4: String = String::from("Initial Contents");
 
+    // Demonstrate string concatenation
     let mut foo_bar: String = String::from("Foo ");
-    // Takes in &str / string slice
     foo_bar.push_str("Bar");
-    // Takes in char
     foo_bar.push('!');
 
+    // Demonstrate string concatenation using the + operator and the format! macro
     let hello = String::from("Hello, ");
     let world = String::from("World!");
-    // One Way
     let hello_world_1 = hello + &world;
-    // Another Way
     // let hello_world_2 = format!("{}{}", hello, world);
-
-    // It doesn't work
-    // let c: char = hello[0];
-
-    // What will work then?
 }
 
+// Function to demonstrate HashMap operations
 fn h_maps() {
+    // Declare some strings
     let team_1: String = String::from("Mumbai Indians");
     let team_2: String = String::from("Hyderabad");
 
+    // Declare a mutable HashMap and insert some key-value pairs
     let mut hash_map: HashMap<String, i32> = HashMap::new();
-
     hash_map.insert(team_1, 10);
     hash_map.insert(team_2, 14);
 
+    // Access a value in the HashMap
     let val: Option<&i32> = hash_map.get(&String::from("Hyderabad"));
 
+    // Use match to handle potential missing key
     match val {
         Some(14) => println!("It is what they scored"),
         None => println!("Didn't they score something?"),
         _ => println!("They ain't gonna score nothing"),
     }
 
-    // Extrac Key & Valkue (KV) from the hash map
+    // Iterate over the key-value pairs in the HashMap
     for (key, val) in &hash_map {
         println!("{} - {}", key, val);
     }
 
+    // Insert a new key-value pair and demonstrate the entry API
     hash_map.insert(String::from("Hyderabad"), 15);
-
-    // If entry is found then fetch it, or insert the new value with that as the key and default value
     hash_map
         .entry(String::from("Rajasthan Royals"))
         .or_insert(6);
 }
 
+// Function to demonstrate counting occurrences of words in a string
 fn problem() {
     let quote = "Hello world wonderful world";
 
+    // Declare a mutable HashMap to store word counts
     let mut hash_map: HashMap<&str, i32> = HashMap::new();
 
+    // Split the string into words and count the occurrences of each word
     let quote_arr = quote.split_whitespace();
-
     for word in quote_arr {
         let count = hash_map.entry(word).or_insert(0);
         *count += 1;
     }
 
+    // Print the word counts
     println!("{:?}", hash_map);
 }
